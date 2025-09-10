42964d762eb40d36773889eec3076e2cd48de0d8
vhahangwele <140509423+Vhahangwele07@users.noreply.github.com>
2025-09-10 14:56:03 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // export const api = 'https://tech-wise-server-oa2v-git-main-memory-iceps-projects.vercel.app/api/';
 // export const api = 'https://tech-wise-server-xvlu.onrender.com/api/';
 
-export const api = 'https://tech-wise-server-brown.vercel.app/api/';
-//xport const api = 'http://localhost:3000/api/';
+//export const api = 'https://tech-wise-server-brown.vercel.app/api/';
+export const api = 'http://localhost:3000/api/';
 
 export const BASE_URL = 'https://socketserver-k62n.onrender.com';
